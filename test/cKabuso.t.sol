0eded89afd4053619eceade7d8b558a8c063f3c1
nah <test@mail.com>
2024-05-26 11:15:51 +0200

    last min test fixes

--- test/cKabuso.t.sol
+++ test/cKabuso.t.sol
@@ -1,45 +1,59 @@
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "lib/forge-std/src/Test.sol";
 import {Verifier} from "src/Verifier.sol";
 import {IHasher} from "src/MerkleTreeWithHistory.sol";
 import {HuffDeployer} from "lib/foundry-huff/src/HuffDeployer.sol";
 import {ETHKabuso} from "src/ETHKabuso.sol";
 
 contract KabusoTest is Test {
-    EthKabuso kabuso;
-
+    ETHKabuso kabuso;
     IHasher deployedMiMCSponge =
         IHasher(0x83584f83f26aF4eDDA9CBe8C730bc87C364b28fe);
     address optimizedMiMCSpongeAddress;
 
+    event Deposit(
+        bytes32 indexed commitment,
+        uint32 leafIndex,
+        uint256 timestamp
+    );
+    event Withdrawal(
+        address to,
+        bytes32 nullifierHash,
+        address indexed relayer,
+        uint256 fee
+    );
+
     function setUp() public {
-        etherFork = vm.createFork("https://api.securerpc.com/v1");
+        uint256 etherFork = vm.createFork("https://api.securerpc.com/v1");
         vm.selectFork(etherFork);
 
         // deploy the EthKabuso Contract
-        tornado = new EthKabuso(
+        kabuso = new ETHKabuso(
             new Verifier(),
             IHasher(0x83584f83f26aF4eDDA9CBe8C730bc87C364b28fe),
             1 ether,
-            20
+            20,
+            address(0x124245)
         );
     }
 
     function testConsole() public pure {
-        console2.log("Hello, World!");
+        console2.log("Setting up deployment successful");
     }
 
     function testDeposit() public {
         // Address to act as the sender
-        address depositor = address(0x123);
+        address depositorA = address(0x123);
+        address depositorB = address(0xffff);
         address withdrawer = address(0x456);
 
         // Set the balance of the depositor address
-        vm.deal(depositor, 10 ether);
+        vm.deal(depositorA, 10 ether);
+        vm.deal(depositorB, 10 ether);
 
         // Start acting as the depositor
-        vm.startPrank(depositor);
+        vm.startPrank(depositorA);
 
         // Call the JavaScript script and get the result
         string[] memory inputs = new string[](2);
@@ -52,10 +66,19 @@ contract KabusoTest is Test {
         // cast commitment to bytes32
         bytes32 testCommitment = commitment;
 
-        // Deposit 1 ether + use testCommitment in deposit call
-        tornado.deposit{value: 1 ether}(testCommitment);
+        vm.recordLogs();
 
-        // ---
+        uint32 leafIndex = 0;
+
+        // Expect the event to be emitted
+        vm.expectEmit(true, true, false, true);
+        emit Deposit(testCommitment, leafIndex, block.timestamp);
+        //bytes32 indexed commitment,uint32 leafIndex,uint256 timestamp
+
+        leafIndex++;
+
+        // Deposit 1 ether + use testCommitment in deposit call
+        kabuso.deposit{value: 1 ether}(testCommitment);
 
         // ---------- WITHDRAW ----------
 
@@ -63,10 +86,13 @@ contract KabusoTest is Test {
         vm.startPrank(withdrawer);
 
         // Call the JavaScript script and get the result
-        string[] memory inputs1 = new string[](2);
-        inputs1[0] = "node";
-        inputs1[1] = "helpers/note.js";
-        /* inputs1[2] = address(withdrawer); */
+        string[] memory noteInputs = new string[](2);
+        noteInputs[0] = "node";
+        noteInputs[1] = "helpers/note.js";
+        /* noteInputs[2] = address(withdrawer); */
+
+        // need to pass a list of recorded deposit events from the contract history to withdraw
+        Vm.Log[] memory entries = vm.getRecordedLogs();
 
         /*  bytes memory noteMem = vm.ffi(inputs);
         bytes32 note = abi.decode(noteMem, (bytes32)); */
